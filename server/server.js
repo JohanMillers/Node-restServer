b22dabcee53d71dad43cc1a09106918d44e37d7b
Johan Millers <johanoscarmarte@gmail.com>
2021-01-29 18:53:38 -0400

    instalacion de bordeParser

--- server/server.js
+++ server/server.js
@@ -1,9 +1,38 @@
 const express = require('express');
-
 const app = express();
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res) => {
-    res.json('Hola Mundo');
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(bodyParser.json())
+
+
+
+
+
+//Get para obtener data
+app.get('/usuario', (req, res) => {
+    res.json('get usuario');
+});
+//Post para crear data
+app.post('/usuario', (req, res) => {
+    
+    res.json('post usuario');
+});
+//Put para actualizar data
+app.put('/usuario/:id', (req, res) => {
+
+    let id = req.params.id;
+
+    res.json({
+        id
+    });
+});
+//Delete para borrar data
+app.delete('/usuario', (req, res) => {
+    res.json('delete usuario');
 });
 
 app.listen(3000, () => {
