a4ed6c7adcea630a5816c812e6c6f63e1e3538f4
Johan Millers <johanoscarmarte@gmail.com>
2021-01-29 19:39:29 -0400

    manejando codigo de errores

--- server/server.js
+++ server/server.js
@@ -21,10 +21,20 @@ app.post('/usuario', (req, res) => {
 
   let body = req.body;
 
+  if (body.nombre === undefined) {
+
+    res.status(400).json({
+      ok: false,
+      mensaje: 'El nombre es necesario'
+    });
+
+  }else {
     res.json({
       persona: body
     });
+  }
 });
+
 //Put para actualizar data
 app.put('/usuario/:id', (req, res) => {
 
