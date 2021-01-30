340dfd2336be046b190087aa3ac28588fd6b0fcd
Johan Millers <johanoscarmarte@gmail.com>
2021-01-30 16:25:16 -0400

    Obteniendo usuario de forma paginada

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,13 +1,41 @@
 
 const express = require('express');
 const bcrypt =  require('bcrypt');
+const _ = require('underscore');
 const Usuario = require('../models/usuario');
 
 const app = express();
 
 //Get para obtener data
 app.get('/usuario', (req, res) => {
-    res.json('get usuario');
+
+    let desde = req.query.desde || 0;
+    desde = Number(desde);
+
+    let limite = req.query.limite || 5;
+    limite = Number(limite);
+
+    Usuario.find({})
+            .skip(desde)
+            .limit(limite)
+            .exec( (err,usuarios) => {
+
+                if(err) {
+                    return res.status(400).json({
+                        ok: false,
+                        err
+
+                    });
+                }
+
+                res.json({
+                    ok: true,
+                    usuarios
+
+                })
+            })
+
+    
 });
 //Post para crear data
 app.post('/usuario', function(req, res) {
@@ -41,9 +69,9 @@ app.post('/usuario', function(req, res) {
 app.put('/usuario/:id', (req, res) => {
 
     let id = req.params.id;
-    let body = req.body;
+    let body = _.pick(req.body, ['nombre','email','img','role','estado']);
 
-    Usuario.findByIdAndUpdate(id,body,{new: true}, (err,usuarioDB) => {
+    Usuario.findByIdAndUpdate(id,body,{new: true, runValidators: true }, (err,usuarioDB) => {
 
         if(err) {
             return res.status(400).json({
