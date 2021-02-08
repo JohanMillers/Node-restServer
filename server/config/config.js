f785003db1f8db3bd9fb627d56358b40d09d31b2
Johan Millers <johanoscarmarte@gmail.com>
2021-02-07 20:21:48 -0400

    Guardando dato en MongoDB Atlas

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,29 @@
 
 
 process.env.PORT = process.env.PORT || 3000;
+
+//=============================
+// Entorno
+//=============================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//=============================
+// Base de datos
+//=============================
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+
+    urlDB = 'mongodb+srv://cafe:123456j@cluster0.gaakx.mongodb.net/cafe?retryWrites=true&w=majority';
+
+}
+
+process.env.URLDB = urlDB;
+
+
+
--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json())
 
 app.use (  require('./routes/usuario') );
 
-mongoose.connect('mongodb://localhost:27017/cafe',(err,res) => {
+mongoose.connect(process.env.URLDB,{useNewUrlParser: true, useUnifiedTopology: true},(err,res) => {
               
         if(err) throw err;
 
